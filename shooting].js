8c6aa48d063fa53fb9108249b9e0bc168c62ba04
rishabhchoudhary140126-eng <rishabhchoudhary140126@gmail.com>
2026-05-19 20:14:38 +0530

    temp buttlet lag fix

--- shooting].js
+++ shooting].js
@@ -43,6 +43,7 @@ document.addEventListener("click", function(){
         dy : dy,
 
         speed : 4,
+        no_of_colision : 4
 
     });
 
@@ -50,10 +51,15 @@ document.addEventListener("click", function(){
 
 
 function renderBullets(){
-
+    console.log(bullets.length);
     for(let i=0; i<bullets.length; i++){
-
+        if(bullets[i].no_of_colision <= 0 || bullets[i].x < 0 || bullets[i].x > canvas.width || bullets[i].y < 0 || bullets[i].y > canvas.height){
+            bullets.splice(i,1);
+            i--;
+            continue;
+}
         for(let j = 0; j< rooms.length; j++){
+            
             for(let k=0; k<rooms[j].walls.length; k++){
                 let x2 = Math.max(rooms[j].walls[k].x2, rooms[j].walls[k].x1);
                 let x1 = Math.min(rooms[j].walls[k].x2, rooms[j].walls[k].x1);
@@ -67,10 +73,12 @@ function renderBullets(){
 
                 if(bullets[i].x>=x1 && bullets[i].x<=x2 && Math.abs(bullets[i].y-y2) <=bullets[i].speed){
                     bullets[i].dy = bullets[i].dy*(-1);
+                    bullets[i].no_of_colision--;
                     break;
                 }
                 else if(bullets[i].y>=y1 && bullets[i].y<=y2 && Math.abs(bullets[i].x-x2)<=bullets[i].speed){
                     bullets[i].dx = bullets[i].dx*(-1);
+                    bullets[i].no_of_colision--;
                     break;
 
 
